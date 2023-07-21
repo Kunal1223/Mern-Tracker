d99c3ca52f006782ec83225717ee8b3035ff3840
Kunal Kishor <kunalbth2002@gmail.com>
2023-07-21 21:13:11 +0530

    router

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const env = require('dotenv');
+const {readdirSync}= require('fs')
 const cors = require("cors") 
 env.config();
 app.use(cors());
 app.use(express.json());
-require('./db/conn')
+require('./db/conn');
 
 const PORT = process.env.PORT;
+
+readdirSync('./routes').map((route) => app.use('/api/v1' , require('./routes/' + route)));
 
 app.get('/' , (req, res)=>{
     res.send("This is the home page for the expense tracker");
