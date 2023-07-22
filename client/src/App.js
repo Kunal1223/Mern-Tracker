a4f1bdd0af1c887c3f78bf05902ebfce9352636e
Kunal Kishor <kunalbth2002@gmail.com>
2023-07-22 15:28:43 +0530

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import { MainLayout } from './style/Layout';
+import Orb from './componet/Orb';
+import Navigation from './Navigation/Navigation';
+import { useMemo, useState } from 'react';
 
 function App() {
+  const [active , setActive] = useState(1);
+  const OrbMemo = useMemo(()=>{
+     return <Orb/>
+  },[])
+
+
   return (
-    <>
-    <h1>This is checking of the git here</h1>
-    </>
+    <AppStyled>
+      {OrbMemo}
+      <MainLayout>
+      <Navigation active ={active} setActive= {setActive} />
+      </MainLayout>
+    </AppStyled>
   );
 }
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
+
 
 export default App;
