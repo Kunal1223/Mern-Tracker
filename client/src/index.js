1eae79c3d2df01d94342b82e6f98b38246772ff8
Kunal Kishor <kunalbth2002@gmail.com>
2023-07-22 17:45:15 +0530

    income add

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { GlobalStyle } from './style/GlobalStyle';
+import { GlobalProvider } from './context/Globalcontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <App />
+    <GlobalStyle />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
 
